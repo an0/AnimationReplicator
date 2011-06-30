ff80025bde9778efb27eb2f7a756cc124883e01f
Ling Wang <an00na@gmail.com>
2011-06-30 09:25:57 -0700

    Initial Commit

---
+++ AnimationReplicator/AnimationReplicatorAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AnimationReplicatorAppDelegate.h
+//  AnimationReplicator
+//
+//  Created by Ling Wang on 6/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AnimationReplicatorViewController;
+
+@interface AnimationReplicatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AnimationReplicatorViewController *viewController;
+
+@end
---
+++ AnimationReplicator/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  AnimationReplicator
+//
+//  Created by Ling Wang on 6/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AnimationReplicatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	int retVal = 0;
+	@autoreleasepool {
+	    retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AnimationReplicatorAppDelegate class]));
+	}
+	return retVal;
+}
